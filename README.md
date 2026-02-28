ff1c1864c974f4010dbfe60a9fe42c1267fd42ed
makichiis <sevans.dev@gmail.com>
2026-02-28 15:49:20 -0600

    Add book catalog solution

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Homework submission and notes monorepo for my systems programming course. Links 
 - [Homework Assignment 1 - Number Analyzer](homework-1-2.rs)
 - [Homework Assignment 1 - Guessing Game](homework-1-3.rs)
 - [Homework Assignment 2 - Word Frequency](homework-2-1.rs)
+- [Homework Assignment 3 - Book Catalog](homework-3-1.rs)
 
---
+++ homework-3-1.rs
@@ -0,0 +1,97 @@
+use std::fs::File;
+use std::fmt::Write;
+use std::io::{Write as OtherWrite, BufReader, BufRead};
+
+#[derive(Debug)]
+struct Book {
+    title: String,
+    author: String,
+    year: u16,
+}
+
+trait CSVSerializable {
+    fn from_csv(csv_string: &str) -> Result<Self, String> where Self: Sized;
+    fn into_csv(&self) -> String;
+}
+
+fn get_csv_escape(s: &String) -> String {
+    s.replace(",", "\\,")
+}
+
+fn get_csv_fields(csv_string: &str) -> Vec<String> {
+    let mut fields: Vec<String> = vec![];
+        
+    let mut escapes: bool = false;
+    for tok in csv_string.split(",") {
+        if escapes && !fields.is_empty() {
+            let _ = write!(fields.last_mut().unwrap(), "{}", tok.replace("\\", ","));
+        } else {
+            fields.push(tok.replace("\\", ","));
+        }
+
+        if !tok.is_empty() {
+            escapes = tok.chars().last() == Some('\\');
+        }
+    }
+
+    fields 
+}
+
+impl CSVSerializable for Book {
+    fn from_csv(csv_string: &str) -> Result<Self, String> {
+        let fields: Vec<String> = get_csv_fields(csv_string);
+
+        if fields.len() != 3 {
+            return Err(format!("Invalid field count. Expected 3, got: {} from '{}'", fields.len(), csv_string));
+        }
+
+        Ok(Book{ title: fields[0].clone(), author: fields[1].clone(), year: fields[2].parse::<u16>().unwrap() })
+    }
+
+    fn into_csv(&self) -> String {
+        let title_escape = get_csv_escape(&self.title);
+        let author_escape = get_csv_escape(&self.author);
+
+        format!("{},{},{}", title_escape, author_escape, self.year)
+    }
+}
+
+fn save_books(books: &Vec<Book>, filename: &str) {
+    let entries: Vec<String> = books.into_iter().map(|book: &Book| book.into_csv()).collect();
+
+    let mut file = File::create(filename).unwrap();
+
+    for entry in entries {
+        let _ = writeln!(file, "{}", entry);
+    }
+}
+
+fn load_books(filename: &str) -> Vec<Book> {
+    let mut retval: Vec<Book> = vec![];
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for entry in reader.lines() {
+        retval.push(Book::from_csv(&entry.unwrap()).unwrap());
+    }
+
+    retval 
+}
+
+fn main() {
+    let books = vec![
+        Book { title: "1984".to_string(), author: "George Orwell".to_string(), year: 1949 },
+        Book { title: "To Kill a Mockingbird".to_string(), author: "Harper Lee".to_string(), year: 1960 },
+        Book { title: "Me, Myself, and I".to_string(), author: "G-Eazy".to_string(), year: 2015 }, 
+    ];
+
+    save_books(&books, "books.txt");
+    println!("Books saved to file.");
+
+    let loaded_books = load_books("books.txt");
+    println!("Loaded books:");
+    for book in loaded_books {
+        println!("{} by {}, published in {}", book.title, book.author, book.year);
+    }
+}
